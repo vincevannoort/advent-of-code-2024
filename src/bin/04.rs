e121d7798a12e9d70b6aca573dabc279033ac329
Vince van Noort <vincevannoort@gmail.com>
2024-12-04 08:13:52 +0100

    feat(day 4): part 1

---
+++ src/bin/04.rs
@@ -0,0 +1,129 @@
+use advent_of_code::{Grid, Location};
+use indoc::indoc;
+use itertools::Itertools;
+use std::collections::HashMap;
+
+advent_of_code::solution!(4);
+
+fn parse(input: &str) -> Grid<char> {
+    // let locations: HashMap<Location, char> = input
+    let locations: HashMap<Location, char> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().map(move |(x, char)| {
+                (
+                    Location {
+                        x: x as u32,
+                        y: y as u32,
+                    },
+                    char,
+                )
+            })
+        })
+        .collect();
+
+    Grid { locations }
+}
+
+fn is_word((a, b, c, d): &(&char, &char, &char, &char)) -> bool {
+    **a == 'X' && **b == 'M' && **c == 'A' && **d == 'S'
+}
+
+fn count_words(chars: &Vec<&char>) -> usize {
+    let counts_forward = chars
+        .clone()
+        .into_iter()
+        .tuple_windows::<(_, _, _, _)>()
+        .filter(is_word)
+        .count();
+    let counts_backward = chars
+        .clone()
+        .into_iter()
+        .rev()
+        .tuple_windows::<(_, _, _, _)>()
+        .filter(is_word)
+        .count();
+    counts_forward + counts_backward
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let grid = parse(input);
+    grid.display();
+
+    let max_location = grid.max_location();
+
+    let mut count = 0;
+
+    // horizontal
+    for y in 0..=max_location.y {
+        let horizontal_row: Vec<&char> = (0..=max_location.x)
+            .filter_map(|x| grid.get(x, y))
+            .collect_vec();
+
+        count += count_words(&horizontal_row);
+    }
+
+    // vertical
+    for x in 0..=max_location.x {
+        let vertical_row = (0..=max_location.y)
+            .filter_map(|y| grid.get(x, y))
+            .collect_vec();
+
+        count += count_words(&vertical_row);
+    }
+
+    // diagonal 1 (left to right up)
+    for y in 0..=(max_location.y + max_location.x) {
+        let diagonal_row: Vec<&char> = (0..=max_location.x)
+            .filter_map(|x| {
+                if x > y {
+                    return None;
+                }
+                grid.get(x, y - x)
+            })
+            .collect_vec();
+
+        count += count_words(&diagonal_row);
+    }
+
+    // diagonal 2 (left to right down)
+    for x in 0..=max_location.x {
+        let diagonal_row: Vec<&char> = (0..=max_location.y)
+            .filter_map(|y| grid.get(x + y, y))
+            .collect_vec();
+
+        count += count_words(&diagonal_row);
+    }
+    // TODO: how can we do this better?
+    for y in 1..=max_location.y {
+        let diagonal_row: Vec<&char> = (0..=max_location.x)
+            .filter_map(|x| grid.get(x, y + x))
+            .collect_vec();
+
+        count += count_words(&diagonal_row);
+    }
+
+    Some(count as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(18));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,42 @@
+use std::collections::HashMap;
+use std::fmt::{Debug, Display};
+
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
+
+#[derive(PartialEq, Clone, Copy, Debug, Eq, Hash, PartialOrd, Ord)]
+pub struct Location {
+    pub x: u32,
+    pub y: u32,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Grid<T> {
+    pub locations: HashMap<Location, T>,
+}
+
+impl<T> Grid<T>
+where
+    T: Debug + Display,
+{
+    pub fn get(&self, x: u32, y: u32) -> Option<&T> {
+        self.locations.get(&Location { x, y })
+    }
+
+    pub fn max_location(&self) -> &Location {
+        let (max_location, _) = self.locations.iter().max_by_key(|x| x.0).unwrap();
+        max_location
+    }
+
+    pub fn display(&self) {
+        let max_location = self.max_location();
+        for y in 0..=max_location.y {
+            for x in 0..=max_location.x {
+                let location = self.locations.get(&Location { x, y }).unwrap();
+                print!("{location}");
+            }
+            println!();
+        }
+    }
+}
