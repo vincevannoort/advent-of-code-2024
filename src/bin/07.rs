f8c9b8e0a7e66e16ffcfa58f10d4abce62fe9320
Vince van Noort <vincevannoort@gmail.com>
2024-12-08 06:33:02 +0100

    feat(day 7): part 1

---
+++ src/bin/07.rs
@@ -0,0 +1,74 @@
+use std::iter::repeat;
+
+use itertools::Itertools;
+
+advent_of_code::solution!(7);
+
+#[derive(PartialEq, Clone, Copy, Debug, Eq, Hash, PartialOrd, Ord)]
+enum Operator {
+    Plus,
+    Times,
+    Concatenation,
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let result: u64 = input
+        .lines()
+        .flat_map(|line| {
+            let (test_value, numbers) = line.split_once(": ").unwrap();
+            let test_value: u64 = test_value.parse().unwrap();
+            let numbers: Vec<u64> = numbers
+                .split_whitespace()
+                .map(|number| number.parse().unwrap())
+                .collect_vec();
+
+            let operator_combinations = repeat([Operator::Plus, Operator::Times])
+                .take(numbers.len() - 1)
+                .multi_cartesian_product()
+                .collect_vec();
+
+            let first_number = numbers.first().unwrap();
+
+            let valid_option = operator_combinations.into_iter().find(|operators| {
+                let result = numbers.iter().skip(1).zip(operators).fold(
+                    *first_number,
+                    |acc, (value, operator)| match operator {
+                        Operator::Plus => acc + value,
+                        Operator::Times => acc * value,
+                        _ => panic!(),
+                    },
+                );
+
+                result == test_value
+            });
+
+            if valid_option.is_some() {
+                Some(test_value)
+            } else {
+                None
+            }
+        })
+        .sum();
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3749));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(11387));
+    }
+}
