1b5443b081b4fae108c1e9ab46a503060469c18f
Vince van Noort <vincevannoort@gmail.com>
2024-12-09 21:26:24 +0100

    feat(day 9): part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -53,8 +53,86 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(result as u64)
 }
 
+fn get_file_groups(values: &[String], size: usize) -> Vec<(String, usize, usize, usize)> {
+    let last_item = values.len() - 1;
+
+    (0..=size)
+        .map(|i| {
+            let start_position = values
+                .iter()
+                .find_position(|c| **c == i.to_string())
+                .unwrap()
+                .0;
+            let end_position = last_item
+                - values
+                    .iter()
+                    .rev()
+                    .find_position(|c| **c == i.to_string())
+                    .unwrap()
+                    .0;
+            (
+                i.to_string(),
+                start_position,
+                end_position,
+                (end_position - start_position) + 1,
+            )
+        })
+        .collect_vec()
+}
+
+fn display_disk(values: &[String]) {
+    values.iter().for_each(|v| print!("{},", v));
+    println!();
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let (mut values, file_groups) = construct_disk(input);
+    let groups = get_file_groups(&values, file_groups);
+
+    // loop over groups starting from the back
+    for (group_value, group_start, group_end, group_size) in groups.iter().rev() {
+        // find free fitting space
+        let free_fitting_space = values
+            .iter()
+            .tuple_windows()
+            .enumerate()
+            .filter(|(i, (a, b))| *a != "." && *b == ".")
+            .find(|(free_space_start, _)| {
+                let free_space_lenght = values
+                    .iter()
+                    .skip(*free_space_start + 1)
+                    .take_while(|c| *c == ".")
+                    .count();
+
+                *group_size <= free_space_lenght
+            })
+            // if we found something, it means we found a (N, .) combination, and thus we need to add 1 to the index
+            .map(|(free_space_start, _)| free_space_start + 1);
+
+        // continue if we did not find one
+        let Some(free_space_start) = free_fitting_space else {
+            continue;
+        };
+
+        // continue if the free space occurs later than the group itself
+        if free_space_start > *group_start {
+            continue;
+        }
+
+        let file_group = *group_start..=*group_end;
+        file_group.into_iter().enumerate().for_each(|(i, file)| {
+            values.swap(free_space_start + i, file);
+        });
+    }
+
+    let result: usize = values
+        .into_iter()
+        .enumerate()
+        .filter(|(i, c)| c != ".")
+        .map(|(i, c)| i * (c.parse::<usize>().unwrap()))
+        .sum();
+
+    Some(result as u64)
 }
 
 #[cfg(test)]
@@ -71,5 +149,45 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(2858));
+    }
+
+    #[test]
+    fn test_part_two_test1() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
+        assert_eq!(result, Some(3317));
+    }
+
+    #[test]
+    fn test_part_two_test2() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(1));
+    }
+
+    #[test]
+    fn test_part_two_test3() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 3,
+        ));
+        assert_eq!(result, Some(4));
+    }
+
+    #[test]
+    fn test_part_two_test4() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 4,
+        ));
+        assert_eq!(result, Some(813));
+    }
+
+    #[test]
+    fn test_part_two_test5() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 5,
+        ));
+        assert_eq!(result, Some(4));
     }
 }
